bd0dc155a315b75430a6ef0c55c65bcd738861f6
JumpyJacko <bobjacklee27@gmail.com>
2025-11-14 09:55:38 +1100

    refactor: extract mainbird to its own component

---
+++ src/lib/components/MainBird.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import { onDestroy, onMount } from "svelte";
+
+    let mainBird: HTMLDivElement;
+    let birdImg: HTMLImageElement;
+
+    function tilt(e: MouseEvent) {
+        const rect = mainBird.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+
+        const rotateY = ((x - centerX) / centerX) * 10;
+        const rotateX = ((y - centerY) / centerY) * -10;
+
+        birdImg.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.10)`;
+    }
+
+    function reset(e: MouseEvent) {
+        birdImg.style.transform = "rotateX(0) rotateY(0) scale(1)";
+    }
+
+    function press(e: MouseEvent) {
+        birdImg.style.transform = "scale(1)";
+    }
+
+    function release(e: MouseEvent) {
+        birdImg.style.transform = "scale(1.1)";
+    }
+
+    onMount(() => {
+        mainBird.addEventListener("mousemove", tilt);
+        mainBird.addEventListener("mouseleave", reset);
+        mainBird.addEventListener("mousedown", press);
+        mainBird.addEventListener("mouseup", release);
+    });
+
+    onDestroy(() => {
+        mainBird.removeEventListener("mousemove", tilt);
+        mainBird.removeEventListener("mouseleave", reset);
+        mainBird.removeEventListener("mousedown", press);
+        mainBird.removeEventListener("mouseup", release);
+    });
+</script>
+
+<div bind:this={mainBird} class="main-bird mt-50">
+    <img
+        bind:this={birdImg}
+        src="/images/birb-bg-no-bg.svg"
+        alt="A monochrome, pixel-art bird"
+        class="w-fit"
+    />
+</div>
+
+<style>
+    .main-bird {
+        height: calc(1 / 4 * 100%);
+        perspective: 1000px;
+    }
+
+    .main-bird img {
+        transition: transform 0.2s ease;
+        transform-style: preserve-3d;
+        pointer-events: none;
+    }
+</style>
+
